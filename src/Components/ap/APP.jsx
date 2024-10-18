8dcbf8183a6acbd6c973cb1fca945e61ee2cfd06
amarasaeed <amarasaeed787@gmail.com>
2024-10-18 18:16:25 +0500

    comit

--- src/Components/ap/APP.jsx
+++ src/Components/ap/APP.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '../../todo/Todo';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,28 +6,66 @@ function APP() {
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
 
-    const handleEdit = () => {
-       
+    useEffect(() => {
+
+        let todoString = localStorage.getItem("todos")
+        if (todoString) {
+            let todos = JSON.parse(todoString)
+            setTodos(todos)
+        }
+    }, [])
+
+
+    const savetolc = () => {
+        localStorage.setItem("todos", JSON.stringify(todos))
     }
 
-    const handleDelete = () => {
-      
+    const handleEdit = (e, id) => {
+        let t = todos.filter(i => i.id === id)
+        setTodo(t[0].todo)
+        let newTodos = todos.filter(item => {
+            return item.id !== id
+        })
+        setTodos(newTodos)
+        savetolc()
+    }
+
+
+    const handleDelete = (e, id) => {
+        let newTodos = todos.filter(item => {
+            return item.id !== id
+        })
+
+        setTodos(newTodos)
+        savetolc()
     }
 
     const handleAdd = () => {
         setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
         setTodo("");
+        savetolc()
     }
 
     const handleChange = (e) => {
         setTodo(e.target.value);
+        savetolc()
+    }
+    const handlecheckbox = (e) => {
+        let id = e.target.name
+        let index = todos.findIndex(item => {
+            return item.id === id
+        })
+        let newTodos = [...todos]
+        newTodos[index].isCompleted = !newTodos[index].isCompleted
+        setTodos(newTodos)
+        savetolc()
     }
 
     return (
         <>
             <Navbar />
-            <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh] md:w-[35%]">
-                <h1 className='font-bold text-center text-3xl'>iTask - Manage your todos at one place</h1>
+            <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh] ">
+
                 <div className="addTodo my-5 flex flex-col gap-4">
                     <h2 className='text-2xl font-bold'>Add a Todo</h2>
                     <div className="flex">
@@ -38,15 +76,17 @@ function APP() {
 
                 <h2 className='text-2xl font-bold'>Your Todos</h2>
                 <div className="todos">
+                    {todos.length === 0 && <div>no todos to display</div>}
                     {todos.map(item => {
-                       return <div key={item.id} className="todo flex">
-                            <div className={item.isCompleted?"":"line-through"}>{item.todo}</div>
+                        return <div key={item.id} className="todo flex">
+                            <input onChange={handlecheckbox} type="checkbox" value={todo.isCompleted} name={item.id} id="" />
+                            <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
                             <div className="buttons">
-                                <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
-                                <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
+                                <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Edit</button>
+                                <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1'>Delete</button>
                             </div>
                         </div>
-})}
+                    })}
                 </div>
             </div>
         </>
